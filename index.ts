64ed395a1ded4f1395b48877d87affb533bf1355
Josh Thornes <jthornes@amazon.com>
2022-03-15 12:59:17 -0600

    update env names

--- index.ts
+++ index.ts
@@ -9,9 +9,9 @@ const app = new cdk.App();
 
 new AppStack(app, process.env.APP_NAME + '-app-stack', {
   appName: process.env.APP_NAME || '',
-  account: process.env.DEV_ACCOUNT || '',
+  account: process.env.ACCOUNT || '',
   env: {
-    account: process.env.DEV_ACCOUNT,
-    region: process.env.DEV_REGION,
+    account: process.env.ACCOUNT,
+    region: process.env.REGION,
   },
 });
